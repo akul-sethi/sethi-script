6d865a0df934322f1c751d1dbdf01a0000a2569c
Akul Sethi <sethi.ak@northeastern.edu>
2023-12-27 19:25:56 +0530

    new repl

--- main.c
+++ main.c
@@ -1,61 +1,59 @@
-#include "common.h"
 #include "chunk.h"
+#include "common.h"
 #include "debug.h"
 #include "vm.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 static void repl() {
-    char line[1024];
+  char line[1024];
 
-    for(;;) {
-        printf("> ");
-        if(!fgets(line, sizeof(line), stdin)) {
-            printf("\n");
-            break;
-        }
-
-        interpret(line);
+  for (;;) {
+    printf(">>>");
+    if (!fgets(line, sizeof(line), stdin)) {
+      printf("\n");
+      break;
     }
+
+    interpret(line);
+  }
 }
 
-static char* readFile(const char* path) {
-    FILE* ptr = fopen(path, "rb");
+static char *readFile(const char *path) {
+  FILE *ptr = fopen(path, "rb");
 
-    fseek(ptr, 0, SEEK_END);
-    int length = ftell(ptr);
-    char* buffer = (char*)malloc(length + 1);
-    rewind(ptr);
+  fseek(ptr, 0, SEEK_END);
+  int length = ftell(ptr);
+  char *buffer = (char *)malloc(length + 1);
+  rewind(ptr);
 
-    size_t bytesRead = fread(buffer, sizeof(char), length, ptr);
-    buffer[bytesRead] = '\0';
+  size_t bytesRead = fread(buffer, sizeof(char), length, ptr);
+  buffer[bytesRead] = '\0';
 
-    fclose(ptr);
-    return buffer;
+  fclose(ptr);
+  return buffer;
 }
 
-static void runFile(const char* path) {
-    char* source = readFile(path);
-    InterpretResult result = interpret(source);
-    free(source);
+static void runFile(const char *path) {
+  char *source = readFile(path);
+  InterpretResult result = interpret(source);
+  free(source);
 
-    if(result == INTERPRET_COMPILE_ERROR) exit(65);
-    if(result == INTERPRET_RUNTIME_ERROR) exit(77);
+  if (result == INTERPRET_COMPILE_ERROR)
+    exit(65);
+  if (result == INTERPRET_RUNTIME_ERROR)
+    exit(77);
 }
 
-
-
-int main(int argc, const char* argv[]) {
-    initVM();
-    if(argc == 1) {
-        repl();
-    } else if (argc == 2) {
-        runFile(argv[1]);
-    } else {
-        fprintf(stderr, "There was an error too many args");
-    }
-    freeVM();    
+int main(int argc, const char *argv[]) {
+  initVM();
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runFile(argv[1]);
+  } else {
+    fprintf(stderr, "There was an error too many args");
+  }
+  freeVM();
 }
-
-
